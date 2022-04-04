5ae870fc89fed12dd5e7aa2a85eb46a74ba5cc47
athanhlc24 <tranvanthanh060699@gmail.com>
2022-04-04 18:11:52 +0700

    Bài Tập Demo

---
+++ Demo2.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+	// Inut a,b;
+	int a;
+	scanf("%d",&a);
+	int b;
+	scanf("%d",&b);
+	
+	// so sanh a > b
+	if(a>b){
+	
+		// Display so lon nhat 
+		printf("a la so lon hon b");
+	}
+	else{
+		if(a==b){
+			printf("hai so bang nhau");	
+			
+		}
+		else{
+			printf("b lon hon a");
+		}
+	}
+		
+	
+}
