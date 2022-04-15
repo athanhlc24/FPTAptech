4e17181f858c22696fbe4ea64499dd01200b630f
athanhlc24 <tranvanthanh060699@gmail.com>
2022-04-15 08:55:09 +0700

    Session 3 - Assignment

---
+++ Session 3- Assignment/Bai1 - Code.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int n;
+	int i = 1;
+	printf("Nhap vao so nguyen n = ");
+	scanf("%d",&n);
+	for(i=1;i<n;i = i+2){
+		printf("\nDanh sach cac so le nho hon n la: %d",i);
+	}
+	
+}
